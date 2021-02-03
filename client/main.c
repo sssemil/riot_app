6c62bdc1244115634ee901920046951e299d05a4
Emil Suleymanov <emil.suleymanov@tum.de>
2021-02-03 01:43:16 +0100

    Basic sending with autoiface selection

--- client/main.c
+++ client/main.c
@@ -1,52 +1,56 @@
-/*
- * Copyright (c) 2015-2016 Ken Bannister. All rights reserved.
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
-
-/**
- * @ingroup     examples
- * @{
- *
- * @file
- * @brief       gcoap example
- *
- * @author      Ken Bannister <kb2ma@runbox.com>
- *
- * @}
- */
-
 #include <stdio.h>
 #include "msg.h"
 
 #include "net/gcoap.h"
-#include "shell.h"
-
-#define MAIN_QUEUE_SIZE (4)
-static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
-
-extern int gcoap_cli_cmd(int argc, char **argv);
-extern void gcoap_cli_init(void);
-
-static const shell_command_t shell_commands[] = {
-    { "coap", "CoAP example", gcoap_cli_cmd },
-    { NULL, NULL, NULL }
-};
+#include "net/sock.h"
+#include "net/netif.h"
 
 int main(void)
 {
-    /* for the thread running the shell */
-    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
-    gcoap_cli_init();
-    puts("gcoap example app");
+    uint8_t buf[CONFIG_GCOAP_PDU_BUF_SIZE];
+    coap_pkt_t pdu;
+    size_t len;
+    char path[] = "/.well-known/core";
 
-    /* start shell */
-    puts("All up, running the shell now");
-    char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    // prepare
+    len = gcoap_request(&pdu, &buf[0], CONFIG_GCOAP_PDU_BUF_SIZE,
+                        COAP_METHOD_GET, &path[0]);
 
-    /* should never be reached */
+    printf("len: %i\n", len);
+
+    // build request
+    char *addr_str = "fe80::7029:f6ff:fe08:af0b";
+    size_t bytes_sent;
+    sock_udp_ep_t remote_mem;
+    sock_udp_ep_t *remote = &remote_mem;
+    ipv6_addr_t addr;
+    remote->family = AF_INET6;
+
+    netif_t *iface = NULL;
+
+    if ((iface = netif_iter(iface)))
+    {
+        char name[CONFIG_NETIF_NAMELENMAX];
+        netif_get_name(iface, name);
+        printf("iface: %s\n", name);
+        remote->netif = netif_get_id(iface);
+    }
+    else
+    {
+        printf("gcoap_client: not interface found\n");
+        return -1;
+    }
+
+    if (ipv6_addr_from_str(&addr, addr_str) == NULL)
+    {
+        printf("gcoap_client: unable to parse destination address\n");
+        return -1;
+    }
+    memcpy(&remote->addr.ipv6[0], &addr.u8[0], sizeof(addr.u8));
+
+    // send
+    bytes_sent = gcoap_req_send(buf, len, remote, NULL, NULL);
+
+    printf("bytes_sent: %i\n", bytes_sent);
     return 0;
 }
