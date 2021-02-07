fad6ba633fbd9722f14934f9f71f35ca4380b40b
Emil Suleymanov <emil.suleymanov@tum.de>
2021-02-07 04:38:02 +0100

    Rewrite server part

--- server/main.c
+++ server/main.c
@@ -1,52 +1,105 @@
-/*
- * Copyright (c) 2015-2016 Ken Bannister. All rights reserved.
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
-
-/**
- * @ingroup     examples
- * @{
- *
- * @file
- * @brief       gcoap example
- *
- * @author      Ken Bannister <kb2ma@runbox.com>
- *
- * @}
- */
-
 #include <stdio.h>
+#include <unistd.h>
+
 #include "msg.h"
 
 #include "net/gcoap.h"
-#include "shell.h"
+#include "net/sock.h"
+#include "net/netif.h"
+#include "benchmark.h"
 
-#define MAIN_QUEUE_SIZE (4)
-static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+static ssize_t _riot_test0_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx);
+static ssize_t _encode_link(const coap_resource_t *resource, char *buf,
+                            size_t maxlen, coap_link_encoder_ctx_t *context);
 
-extern int gcoap_cli_cmd(int argc, char **argv);
-extern void gcoap_cli_init(void);
-
-static const shell_command_t shell_commands[] = {
-    { "coap", "CoAP example", gcoap_cli_cmd },
-    { NULL, NULL, NULL }
+static const coap_resource_t _resources[] = {
+    {"/riot/test0", COAP_GET, _riot_test0_handler, NULL},
 };
+
+static const char *_link_params[] = {
+    NULL};
+
+static gcoap_listener_t _listener = {
+    &_resources[0],
+    ARRAY_SIZE(_resources),
+    _encode_link,
+    NULL,
+    NULL};
+
+/* Adds link format params to resource list */
+static ssize_t _encode_link(const coap_resource_t *resource, char *buf,
+                            size_t maxlen, coap_link_encoder_ctx_t *context)
+{
+    ssize_t res = gcoap_encode_link(resource, buf, maxlen, context);
+    if (res > 0)
+    {
+        if (_link_params[context->link_pos] && (strlen(_link_params[context->link_pos]) < (maxlen - res)))
+        {
+            if (buf)
+            {
+                memcpy(buf + res, _link_params[context->link_pos],
+                       strlen(_link_params[context->link_pos]));
+            }
+            return res + strlen(_link_params[context->link_pos]);
+        }
+    }
+
+    return res;
+}
+
+static ssize_t _riot_test0_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx)
+{
+    (void)ctx;
+    printf("_riot_test0_handler\n");
+    gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+    coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
+    size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
+
+    /* write the RIOT board name in the response buffer */
+    if (pdu->payload_len >= strlen(RIOT_BOARD))
+    {
+        memcpy(pdu->payload, RIOT_BOARD, strlen(RIOT_BOARD));
+        return resp_len + strlen(RIOT_BOARD);
+    }
+    else
+    {
+        printf("gcoap_cli: msg buffer too small\n");
+        return gcoap_response(pdu, buf, len, COAP_CODE_INTERNAL_SERVER_ERROR);
+    }
+}
+
+void print_iface(netif_t *iface)
+{
+    char name[CONFIG_NETIF_NAMELENMAX];
+    netif_get_name(iface, name);
+    printf("iface: %s\n", name);
+
+    ipv6_addr_t ipv6_addr[1];
+
+    if (netif_get_opt(iface, NETOPT_IPV6_ADDR, 0, ipv6_addr, sizeof(ipv6_addr)) >= 0)
+    {
+        char addr_str[40];
+        ipv6_addr_to_str(addr_str, ipv6_addr, sizeof(addr_str));
+        printf("inet6 addr: %s\n", addr_str);
+    }
+}
 
 int main(void)
 {
-    /* for the thread running the shell */
-    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
-    gcoap_cli_init();
-    puts("gcoap example app");
+    printf("======================================================================\n");
+    netif_t *iface = NULL;
 
-    /* start shell */
-    puts("All up, running the shell now");
-    char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    if ((iface = netif_iter(iface)))
+    {
+        print_iface(iface);
+    }
+    else
+    {
+        printf("gcoap_client: not interface found\n");
+        return -1;
+    }
 
-    /* should never be reached */
+    gcoap_register_listener(&_listener);
+
     return 0;
 }
